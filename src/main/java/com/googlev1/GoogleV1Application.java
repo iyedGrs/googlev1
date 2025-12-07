7cb079223d0f3fb4b7385c3e1523f999e391d47b
LouaySassi <sassi.louaycheker@etudiant-fst.utm.tn>
2025-12-07 16:14:56 +0100

    Enable JPA repositories and component scanning

--- src/main/java/com/googlev1/GoogleV1Application.java
+++ src/main/java/com/googlev1/GoogleV1Application.java
@@ -2,8 +2,12 @@ package com.googlev1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.googlev1")
+@EnableJpaRepositories(basePackages = "com.googlev1.repository")
 public class GoogleV1Application {
 
     public static void main(String[] args) {
