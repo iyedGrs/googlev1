abd478cb59106a6fcd41cb787328b38b7ee0e016
iyed grassi <IGrassi@se.linedata.com>
2025-12-20 20:17:22 +0100

    added both servicces and database repository for search

--- src/main/java/com/googlev1/GoogleV1Application.java
+++ src/main/java/com/googlev1/GoogleV1Application.java
@@ -2,10 +2,10 @@ package com.googlev1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class GoogleV1Application {
 
     public static void main(String[] args) {
